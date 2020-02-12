e78da3398209ab92459e6a56f70b987fefbcf9ab
Héctor Serrano <fonto.serrano@gmail.com>
2020-02-11 21:25:59 -0600

    grid in place

--- src/components/imageList/ImageList.js
+++ src/components/imageList/ImageList.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import './ImageList.scss'
+import React from "react";
+import "./ImageList.scss";
 
-const ImageList = ( {data} ) => {
-    return(
-        <>
-            {data.map(value => (
-                <div key={value.id} className="container">
-                    <p>{value.tags}</p>
-                </div>
-            ))}
-        </>
-    )
-}
+const ImageList = ({ data }) => {
+	return (
+		<>
+			<div className="container-grid">
+				{data.map(value => (
+					<div className="card">
+						<img src={value.userImageURL} alt={value.type}></img>
+						<p>{value.tags}</p>
+					</div>
+				))}
+			</div>
+		</>
+	);
+};
 
 export default ImageList;
