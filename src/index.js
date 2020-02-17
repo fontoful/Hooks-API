6a122edb7e5f5111297c75e30833b2fa7f86c16a
GSGranados <stevengranados0211@gmail.com>
2020-02-17 04:49:28 -0600

    ImageView implementation
    With React Router Dom it was created a component to see all the information related to one image, the author, download link, UserImageURL and a back button to home.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+//import App from './App';
+import Router from './components/Router/Router';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Router />, document.getElementById('root'));
