6b5088162dce430f0af48fbf0222b21853d28a9d
Héctor Serrano <fonto.serrano@gmail.com>
2020-02-19 21:21:31 -0600

    fix

--- src/components/ImageView/ImageView.js
+++ src/components/ImageView/ImageView.js
@@ -3,29 +3,31 @@ import { Link } from "react-router-dom";
 import "./ImageView.scss";
 
 const ImageView = props => {
-  console.log(props.location.state.value);
-  const {
-    userImageURL: image,
-    tags,
-    user: owner,
-    pageURL
-  } = props.location.state.value;
-  return (
-    <div className="imageContainer">
-      <img className="image" src={image} alt={tags}></img>
-      <h4>
-        Credits: <span>{owner}</span>
-      </h4>
-      <button className="downloadButton">
-        <span>
-          <a target="_blank" href={pageURL}>
-            Go to the Download Page
-          </a>
-        </span>
-      </button>
-      <Link to={{pathname: "/"}}><button className="homeButton">Home</button></Link>
-    </div>
-  );
+	console.log(props.location.state.value);
+
+	const {
+		userImageURL: image,
+		tags,
+		user: owner,
+		pageURL: URL
+	} = props.location.state.value;
+
+	return (
+		<div className="imageContainer">
+			<img className="image" src={image} alt={tags}></img>
+			<h4>
+				Credits: <span>{owner}</span>
+			</h4>
+			<button className="downloadButton">
+				<span>
+					<a href={URL}>Go to the Download Page</a>
+				</span>
+			</button>
+			<Link to={{ pathname: "/" }}>
+				<button className="homeButton">Home</button>
+			</Link>
+		</div>
+	);
 };
 
 export default ImageView;
